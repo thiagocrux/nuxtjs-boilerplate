35ad4df00d9ce3b8b43145b86a8499983967ee44
Thiago Cruz <thiagocruz.eu@gmail.com>
2024-11-05 20:43:22 -0300

    chore: config tailwind module

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,7 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
   srcDir: 'src/',
-  modules: ['@nuxt/eslint', '@nuxtjs/storybook'],
+  modules: ['@nuxt/eslint', '@nuxtjs/storybook', '@nuxtjs/tailwindcss'],
   vite: {
     optimizeDeps: {
       include: ['storybook > @storybook/core > jsdoc-type-pratt-parser'],
